bbd5f220cb7234311c2a9ebc378292651348a24d
Milo Girardet <57747754+mgirar84@users.noreply.github.com>
2024-02-22 10:49:24 +0000

    fix card bookmark color and update primary theme

--- src/components/atoms/icons/types.ts
+++ src/components/atoms/icons/types.ts
@@ -3,4 +3,5 @@ import type { CSSProperties, HTMLAttributes } from "react";
 export type IconProps = {
   isActive?: boolean;
   color?: CSSProperties["color"];
+  isActiveColor?: CSSProperties["color"];
 } & HTMLAttributes<HTMLOrSVGElement>;
